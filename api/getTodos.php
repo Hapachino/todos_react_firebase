cb6d39e04c5c4bbb7ecde7ab1d36c87b2f8797b1
Hapachino <erickwbrownfield@gmail.com>
2019-02-18 23:54:29 -0800

    basic getTodos complete

--- api/getTodos.php
+++ api/getTodos.php
@@ -9,20 +9,22 @@ $userId = 1;
 $getTodosQuery->bind_param("i", $userId);
 $getTodosQuery->execute();
 
-// $getTodosQuery->store_result();
-// $getTodosQuery->bind_result($idRow, $userIdRow, $todoTextRow, $completedRow);
+$getTodosQuery->bind_result($id, $userId, $todoText, $completed);
 
-// while ($getTodosQuery->fetch()) {
-//   $ids[] = $idRow;
-//   $userIds[] = $userIdRow;
-//   $todoTexts[] = $todoTextRow;
-//   $completeds[] = $completedRow;
-// }
+$todos = [];
 
-// $todos = $getTodosQuery->get_result();
+while ($getTodosQuery->fetch()) {
+  $todo = [];
+  $todo['id'] = $id;
+  $todo['userId'] = $userId;
+  $todo['todoText'] = $todoText;
+  $todo['completed'] = $completed;
+
+  array_push($todos, $todo);
+}
 
 $link->close();
 
-print(json_encode($todoTexts));
+print(json_encode($todos));
 
 ?>
