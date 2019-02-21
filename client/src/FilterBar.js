b2111fc9983c041e2eb2c831b25c249c7888a8a8
Hapachino <erickwbrownfield@gmail.com>
2019-02-20 20:52:57 -0800

    working radio buttons, pending functionality

--- client/src/FilterBar.js
+++ client/src/FilterBar.js
@@ -8,25 +8,64 @@ class FilterBar extends Component {
     this.state = {
       filter: 'all',
     }
+
+    this.options = [
+      {
+        label: 'All',
+        value: 'all',
+      },
+      {
+        label: 'Active',
+        value: 'active',
+      },
+      {
+        label: 'Completed',
+        value: 'completed',
+      }
+    ]
+  }
+
+  renderRadioButtons = options => {
+    const { state: { filter }, handleOnChange } = this;
+
+    const radioButtons = options.map((option, index) => {
+      const { label, value } = option;
+
+      return (
+        <label>
+          <input
+            key={index}
+            className="with-gap"
+            name="filter"
+            value={value}
+            type="radio"
+            checked={filter === value}
+            onChange={handleOnChange}
+          />
+          <span>{label}</span>
+        </label>
+      );
+    });
+
+    return (
+      <form className="col m6 offset-m3 space-evenly">
+        {radioButtons}
+      </form>
+    );
+  }
+
+  handleOnChange = e => {
+    this.setState({
+      filter: e.target.value,
+    });
   }
 
   render() {
+    const { options, renderRadioButtons } = this;
+
     return (
       <div className="row">
-        <form className="col m6 offset-m3 space-evenly">
-          <label>
-            <input className="with-gap" name="filter" value="all" type="radio" checked />
-            <span>All</span>
-          </label>
-          <label>
-            <input className="with-gap" name="filter" value="active" type="radio" />
-            <span>Active</span>
-          </label>
-          <label>
-            <input className="with-gap" name="filter" value="completed" type="radio" />
-            <span>Completed</span>
-          </label>
-        </form>
+        {renderRadioButtons(options)}
       </div>
     );
   }
