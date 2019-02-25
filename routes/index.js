e88dec33bfd1c66ebc739b5ed38e97388a95dd99
Hapachino <erickwbrownfield@gmail.com>
2019-02-24 16:14:35 -0800

    restructured folders and deleted unneeded files

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const { resolve } = require('path');
-
-module.exports = app => {
-  app.use('/api', require('./api'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
