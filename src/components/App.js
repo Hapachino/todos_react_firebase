a3d38e0f7cd137e753bacbe5cc680a8553af23cd
Hapachino <erickwbrownfield@gmail.com>
2019-02-26 20:13:53 -0800

    extracted dashboard from app to own component

--- src/components/App.js
+++ src/components/App.js
@@ -1,94 +1,25 @@
-import React, { Component, Fragment } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import { getTodos } from '../action';
-import db from '../firebase';
-import InputBar from './InputBar';
+import Dashboard from './Dashboard';
 import Navbar from './Navbar';
-import FilterBar from './FilterBar';
-import TodoList from './TodoList';
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      todoText: '',
-      filter: 'all',
-    }
-  }
-
-  async componentDidMount() {
-    this.dbRef = this.props.getTodos();
-  }
-
-  addTodo = async e => {
-    const { state: { todoText } } = this;
-    const userId = 1;
-
-    if (e.key === 'Enter' && todoText.trim()) {
-      db.ref('/todos/' + userId).push({
-        todoText,
-        completed: false,
-      });
-
-      this.setState({
-        todoText: '',
-      });
-    }
-  }
-
-  deleteTodo = async id => {
-    const userId = 1;
-    
-    await db.ref(`/todos/${userId}/${id}`).remove();
-  }
-
-  handleFilterChange = e => {
-    this.setState({
-      filter: e.target.value,
-    });
-  }
-
-  handleTextChange = e => {
-    this.setState({
-      todoText: e.target.value,
-    });
-  };
-
   render() {
-    const { state: { todoText, filter }, handleFilterChange, handleTextChange, addTodo, deleteTodo } = this;
-    
     return (
-      <Fragment>
-        <Navbar />
-        <div className="container">
-          <h1 className="center red-text">todos</h1>
-          <InputBar
-            todoText={todoText}
-            handleTextChange={handleTextChange}
-            addTodo={addTodo}
-          />
-          <FilterBar
-            filter={filter}
-            handleFilterChange={handleFilterChange}
-          />
-          <TodoList
-            todos={this.props.todos}
-            filter={filter}
-            deleteTodo={deleteTodo}
-          />
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Dashboard} />
+            </Switch>
+          </div>
         </div>
-      </Fragment>
+      </Router>
     );
   }
 }
 
-const mapStateToProps = state => ({
-  todos: state.todos.todos,
-});
-
-export default connect(mapStateToProps, {
-  getTodos,
-})(App);
+export default App;
---
+++ src/components/Dashboard.js
@@ -0,0 +1,92 @@
+import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import { getTodos } from '../action';
+import db from '../firebase';
+import InputBar from './InputBar';
+import Navbar from './Navbar';
+import FilterBar from './FilterBar';
+import TodoList from './TodoList';
+import './App.css';
+
+class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todoText: '',
+      filter: 'all',
+    }
+  }
+
+  async componentDidMount() {
+    this.dbRef = this.props.getTodos();
+  }
+
+  addTodo = async e => {
+    const { state: { todoText } } = this;
+    const userId = 1;
+
+    if (e.key === 'Enter' && todoText.trim()) {
+      db.ref('/todos/' + userId).push({
+        todoText,
+        completed: false,
+      });
+
+      this.setState({
+        todoText: '',
+      });
+    }
+  }
+
+  deleteTodo = async id => {
+    const userId = 1;
+
+    await db.ref(`/todos/${userId}/${id}`).remove();
+  }
+
+  handleFilterChange = e => {
+    this.setState({
+      filter: e.target.value,
+    });
+  }
+
+  handleTextChange = e => {
+    this.setState({
+      todoText: e.target.value,
+    });
+  };
+
+  render() {
+    const { state: { todoText, filter }, handleFilterChange, handleTextChange, addTodo, deleteTodo } = this;
+
+    return (
+      <div>
+        <h1 className="center red-text">todos</h1>
+        <InputBar
+          todoText={todoText}
+          handleTextChange={handleTextChange}
+          addTodo={addTodo}
+        />
+        <FilterBar
+          filter={filter}
+          handleFilterChange={handleFilterChange}
+        />
+        <TodoList
+          todos={this.props.todos}
+          filter={filter}
+          deleteTodo={deleteTodo}
+        />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  todos: state.todos.todos,
+});
+
+export default connect(mapStateToProps, {
+  getTodos,
+})(Dashboard);
