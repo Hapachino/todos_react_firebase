49cd6322edfec25e0ef31700f61996e8712016f9
Hapachino <erickwbrownfield@gmail.com>
2019-02-27 18:32:26 -0800

    basic firebase login

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { auth } from '../firebase';
 
 class SignIn extends Component {
   state = {
     email: '',
     password: '',
+    error: '',
   }
 
   handleTextChange = e => {
@@ -13,12 +15,28 @@ class SignIn extends Component {
     });
   }
 
-  render() {
-    const { handleTextChange } = this;
+  handleSubmit = e => {
+    e.preventDefault();
+    const { email, password } = this.state;
 
+    auth.signInWithEmailAndPassword(email, password)
+        .then(user => {
+          this.props.history.push('/');
+          console.log(user);
+        })
+        .catch(error => {
+          this.setState({ error: error });
+
+          console.log(error);
+        });
+  };
+
+  render() {
+    const { handleSubmit, handleTextChange } = this;
+    
     return (
       <div className="container">
-        <form>
+        <form onSubmit={handleSubmit}>
           <h5 className="grey-text text-darken-3">Sign In</h5>
           <div className="input-field">
             <label htmlFor="email">Email</label>
@@ -27,6 +45,11 @@ class SignIn extends Component {
           <div className="input-field">
             <label htmlFor="password">Password</label>
             <input type="password" id="password" onChange={handleTextChange} />
+          </div>
+          <div className="input-field">
+            <button className="btn blue lighten-1">
+              Sign In
+            </button>
           </div>
         </form>
       </div>
--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,7 +1,11 @@
 import firebase from 'firebase/app';
+import 'firebase/auth';
 import 'firebase/database';
 import firebaseConfig from '../config/firebase';
 
 firebase.initializeApp(firebaseConfig);
 
 export default firebase.database();
+
+const auth = firebase.auth();
+export { auth };
