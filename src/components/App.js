7cdc1a520fd765659611f1d7b97892c4f0f917bb
Hapachino <erickwbrownfield@gmail.com>
2019-02-27 17:48:39 -0800

    login basic styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import './App.css';
 import Dashboard from './Dashboard';
 import Navbar from './Navbar';
-import './App.css';
+import SignIn from './SignIn';
 
 class App extends Component {
   render() {
@@ -14,6 +15,7 @@ class App extends Component {
           <div className="container">
             <Switch>
               <Route exact path="/" component={Dashboard} />
+              <Route path="/signin" component={SignIn} />
             </Switch>
           </div>
         </div>
