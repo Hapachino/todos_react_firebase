8e34e3d087c3bcc3319679c29a07843df6d82ea0
Hapachino <erickwbrownfield@gmail.com>
2019-03-03 16:54:17 -0800

    applied auth hoc to route components

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import './App.css';
+import auth from '../hoc/auth';
 import Dashboard from './Dashboard';
 import Navbar from './Navbar';
 import SignIn from './SignIn';
@@ -15,9 +16,9 @@ class App extends Component {
           <Navbar />
           <div className="container">
             <Switch>
-              <Route exact path="/" component={Dashboard} />
-              <Route path="/signin" component={SignIn} />
-              <Route path="/signup" component={SignUp} />
+              <Route exact path="/" component={auth(Dashboard)} />
+              <Route path="/signin" component={auth(SignIn, '/', true)} />
+              <Route path="/signup" component={auth(SignUp, '/', true)} />
             </Switch>
           </div>
         </div>
