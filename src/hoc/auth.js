b08bdac999e4999ff297e545c6248e91044c5c9c
Hapachino <erickwbrownfield@gmail.com>
2019-03-03 16:49:32 -0800

    added auth hoc

---
+++ src/hoc/auth.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+export default (WrappedComponent, to = '/signin', redirect = false) => {
+  class Auth extends Component {
+    componentDidMount() {
+      this.checkAuth();
+    }
+
+    componentDidUpdate() {
+      this.checkAuth();
+    }
+
+    checkAuth() {
+      const { auth, history } = this.props;
+
+      if (auth === redirect) {
+        history.push(to);
+      }
+    }
+
+    render() {
+      return <WrappedComponent {...this.props} />;
+    }
+  }
+
+  return connect(mapStateToProps)(Auth);
+}
+
+
+const mapStateToProps = state => ({
+  auth: state.users.auth,
+});
